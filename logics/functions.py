d7b7bc4cc180ffc7179330925942e7e258fe149e
skihyeon1001 <hero981001@gmail.com>
2025-05-21 01:29:34 +0000

    pytest 라이브러리를 이용한 test case 작성.

---
+++ logics/functions.py
@@ -0,0 +1,36 @@
+
+
+def generate_next_term_from_current_term(current_term: str) -> str:
+    """
+    args:
+        current_term: str
+    returns:
+        next_term: str
+    
+    current_term의 각 문자를 조회하여 연속된 문자의 개수와 문자를 카운트하여 다음 항을 생성.
+    """
+    pass
+
+def get_nth_term(n: int) -> str:
+    """
+    args:
+        n: int
+    returns:
+        nth_term: str
+    
+    n번째 개미수열 항을 생성.
+    """
+    pass
+
+def main(n: int) -> str:
+    """
+    args:
+        n: int
+    returns:
+        nth_term: str
+    
+    양의 정수 n이 주어질때  n번째 항(Ln)의 자릿수 중 가운데 두 자리 수(m)를 출력.
+    """
+    pass
+
+
